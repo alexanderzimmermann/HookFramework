81994498be3be52ff806c729a573e724bf7bd749
alexanderzimmermann <alex@azimmermann.com>
2013-05-25 20:42:46 +0200

    New Response object to be returned to VCS.

---
+++ library/Hook/Core/Response.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * A simple response object.
+ * @category   Core
+ * @package    Main
+ * @subpackage Main
+ * @author     Alexander Zimmermann <alex@azimmermann.com>
+ * @copyright  2008-2013 Alexander Zimmermann <alex@azimmermann.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link       http://www.azimmermann.com/
+ * @since      File available since Release 3.0.0
+ */
+
+namespace Hook\Core;
+
+/**
+ * A simple response object.
+ * @category   Core
+ * @package    Main
+ * @subpackage Main
+ * @author     Alexander Zimmermann <alex@azimmermann.com>
+ * @copyright  2008-2013 Alexander Zimmermann <alex@azimmermann.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: 3.0.0
+ * @link       http://www.azimmermann.com/
+ * @since      Class available since Release 3.0.0
+ */
+class Response
+{
+    /**
+     * Stream output goes to.
+     * @var resource
+     */
+    private $rStream = STDERR;
+
+    /**
+     * The response result.
+     * @var integer
+     */
+    private $iResult = 1;
+
+    /**
+     * The text to be send to the outplut.
+     * @var string
+     */
+    private $sText = 'no text';
+
+    /**
+     * Cosntructor
+     * @author Alexander Zimmermann <alex@azimmermann.com>
+     */
+    public function __construct($rStream = STDERR)
+    {
+        $this->rStream = $rStream;
+    }
+
+    /**
+     * Set the result. Only 0 and 1 at the moment. Other values will be ignored.
+     * @author Alexander Zimmermann <alex@azimmermann.com>
+     */
+    public function setResult($iResult)
+    {
+        // As 0 stands for success we want to be save that a rue 0 is given.
+        if (false === is_int($iResult)) {
+
+            $iResult = 1;
+        }
+
+        $iResult = (int) $iResult;
+
+        if ((0 === $iResult) || (1 === $iResult)) {
+
+            $this->iResult = $iResult;
+        }
+    }
+
+    /**
+     * Get the result to be send to the VCS.
+     * @author Alexander Zimmermann <alex@azimmermann.com>
+     */
+    public function getResult()
+    {
+        return $this->iResult;
+    }
+
+    /**
+     * Set the text.
+     * @param string $sText The text to be send.
+     * @author Alexander Zimmermann <alex@azimmermann.com>
+     */
+    public function setText($sText)
+    {
+        $this->sText = $sText;
+    }
+
+    /**
+     * Get the text.
+     * @return string
+     * @author Alexander Zimmermann <alex@azimmermann.com>
+     */
+    public function getText()
+    {
+        return $this->sText;
+    }
+
+    /**
+     * Send the text to the VCS.
+     * @author Alexander Zimmermann <alex@azimmermann.com>
+     */
+    public function send()
+    {
+        fwrite($this->rStream, $this->sText);
+    }
+}
---
+++ tests/HookTest/Core/ResponseTest.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Tests for response object.
+ * @category   Tests
+ * @package    Main
+ * @subpackage Core
+ * @author     Alexander Zimmermann <alex@azimmermann.com>
+ * @copyright  2008-2013 Alexander Zimmermann <alex@azimmermann.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link       http://www.azimmermann.com/
+ * @since      File available since Release 3.0.0
+ */
+
+namespace HookTest\Core;
+
+use Hook\Core\Response;
+
+require_once __DIR__ . '/../../Bootstrap.php';
+
+/**
+ * Tests for response object.
+ * @category   Tests
+ * @package    Main
+ * @subpackage Core
+ * @author     Alexander Zimmermann <alex@azimmermann.com>
+ * @copyright  2008-2013 Alexander Zimmermann <alex@azimmermann.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: 3.0.0
+ * @link       http://www.azimmermann.com/
+ * @since      Class available since Release 3.0.0
+ */
+class ResponseTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Response object.
+     * @var Response
+     */
+    protected $oResponse;
+
+    /**
+     * Setup
+     * @author Alexander Zimmermann <alex@azimmermann.com>
+     */
+    protected function setUp()
+    {
+        $this->oResponse = new Response();
+    } // function
+
+    /**
+     * Test check values on create
+     * @author Alexander Zimmermann <alex@azimmermann.com>
+     */
+    public function testCreate()
+    {
+        $this->assertSame('no text', $this->oResponse->getText());
+        $this->assertSame(1, $this->oResponse->getResult());
+    }
+
+    /**
+     * Set response result to success.
+     * @author Alexander Zimmermann <alex@azimmermann.com>
+     */
+    public function testSetSuccess()
+    {
+        $this->oResponse->setResult(0);
+        $this->oResponse->setText('');
+
+        $this->assertSame(0, $this->oResponse->getResult());
+        $this->assertSame('', $this->oResponse->getText());
+    }
+
+    /**
+     * Set a wrong value for result.
+     * @author Alexander Zimmermann <alex@azimmermann.com>
+     */
+    public function testSetWrongValueForResult()
+    {
+        $this->oResponse->setResult('abc123');
+
+        $this->assertSame(1, $this->oResponse->getResult());
+    }
+
+    /**
+     * Test the send output.
+     * @author Alexander Zimmermann <alex@azimmermann.com>
+     */
+    public function testSend()
+    {
+        $sFile = __DIR__ . '/_files/send.file';
+        $rFile = fopen($sFile, 'w+');
+        $this->oResponse = new Response($rFile);
+
+        $sMsg = "Failure during commit!\nComment is missing";
+        $this->oResponse->setText($sMsg);
+        $this->oResponse->send();
+
+        $this->assertFileEquals(__DIR__ . '/_files/expected.file', $sFile);
+    }
+}
