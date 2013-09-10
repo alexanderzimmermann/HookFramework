37598b5e82ed0a8e734a186c1c3ae62840c3332b
alexanderzimmermann <alex@azimmermann.com>
2013-09-10 22:39:17 +0200

    New tests for usage messages.

---
+++ tests/HookTest/Adapter/Git/UsageTest.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * Usage-Object Tests.
+ * @category   Tests
+ * @package    Adapter
+ * @subpackage Git
+ * @author     Alexander Zimmermann <alex@azimmermann.com>
+ * @copyright  2008-2013 Alexander Zimmermann <alex@azimmermann.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    PHP 5.4
+ * @link       http://www.azimmermann.com/
+ * @since      File available since Release 3.0.0
+ */
+
+namespace HookTest\Adapter\Git;
+
+use Hook\Adapter\Git\Usage;
+
+require_once __DIR__ . '/../../../Bootstrap.php';
+
+/**
+ * Usage-Object Tests.
+ * @category   Tests
+ * @package    Adapter
+ * @subpackage Git
+ * @author     Alexander Zimmermann <alex@azimmermann.com>
+ * @copyright  2008-2013 Alexander Zimmermann <alex@azimmermann.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: 3.0.0
+ * @link       http://www.azimmermann.com/
+ * @since      Class available since Release 3.0.0
+ */
+class UsageTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test client commit msg usage
+     * @author Alexander Zimmermann <alex@azimmermann.com>
+     */
+    public function testClientCommitMsg()
+    {
+        $this->markTestIncomplete('not implemented yet');
+    }
+
+    /**
+     * Test client prepare-commit-msg
+     * @author Alexander Zimmermann <alex@azimmermann.com>
+     */
+    public function testClientPrepareCommitMsg()
+    {
+        $this->markTestIncomplete('not implemented yet');
+    }
+
+    /**
+     * Test for PreCommit.
+     * @return void
+     * @author Alexander Zimmermann <alex@azimmermann.com>
+     */
+    public function testUsageClientPreCommit()
+    {
+        $oUsage = new Usage('client', 'pre-commit');
+
+        // Expected text.
+        $sExpected = 'Call with the following parameters and order:' . "\n";
+        $sExpected .= "\n";
+        $sExpected .= 'REPOSITORY Repository path (/path/to/project)' . "\n";
+        $sExpected .= '           REPOSITORY=$(git rev-parse --show-toplevel)' . "\n";
+        $sExpected .= 'Commit     HEAD, SHA1' . "\n";
+        $sExpected .= 'Hook       client.pre-commit' . "\n";
+        $sExpected .= "\n";
+        $sExpected .= 'Example: ';
+        $sExpected .= '/path/to/hookframework/Hook $REPOSITORY Commit client.pre-commit' . "\n";
+
+        $this->assertEquals($sExpected, $oUsage->getUsage());
+    }
+
+    /**
+     * Test for PostCommit.
+     * @return void
+     * @author Alexander Zimmermann <alex@azimmermann.com>
+     */
+    public function testUsageClientPostCommit()
+    {
+        $oUsage = new Usage('client', 'post-commit');
+
+        // Expected text.
+        $sExpected = 'Call with the following parameters and order:' . "\n";
+        $sExpected .= "\n";
+        $sExpected .= 'REPOSITORY Repository path (/path/to/project)' . "\n";
+        $sExpected .= '           REPOSITORY=$(git rev-parse --show-toplevel)' . "\n";
+        $sExpected .= 'Commit     HEAD, SHA1' . "\n";
+        $sExpected .= 'Hook       client.post-commit' . "\n";
+        $sExpected .= "\n";
+        $sExpected .= 'Example: ';
+        $sExpected .= '/path/to/hookframework/Hook $REPOSITORY Commit client.post-commit' . "\n";
+
+        $this->assertEquals($sExpected, $oUsage->getUsage());
+    }
+
+    /**
+     * Test for PostCommit.
+     * @return void
+     * @author Alexander Zimmermann <alex@azimmermann.com>
+     */
+    public function testUsageClientCommitMsg()
+    {
+        $oUsage = new Usage('client', 'commit-msg');
+
+        // Expected text.
+        $sExpected = 'Call with the following parameters and order:' . "\n";
+        $sExpected .= "\n";
+        $sExpected .= 'REPOSITORY Repository path (/path/to/project)' . "\n";
+        $sExpected .= '           REPOSITORY=$(git rev-parse --show-toplevel)' . "\n";
+        $sExpected .= 'Commit     HEAD, SHA1' . "\n";
+        $sExpected .= 'Hook       client.commit-msg' . "\n";
+        $sExpected .= "\n";
+        $sExpected .= 'Example: ';
+        $sExpected .= '/path/to/hookframework/Hook $REPOSITORY $commit $commitmsgfile client.commit-msg' . "\n";
+
+        $this->assertEquals($sExpected, $oUsage->getUsage());
+    }
+
+    /**
+     * Test for common usage.
+     * @return void
+     * @author Alexander Zimmermann <alex@azimmermann.com>
+     */
+    public function testUsageCommon()
+    {
+        $oUsage = new Usage('client', 'something');
+
+        // Expected text.
+        $sExpected = 'Call with the following parameters and order:' . "\n";
+        $sExpected .= "\n";
+        $sExpected .= 'REPOSITORY Repository path (/path/to/project)' . "\n";
+        $sExpected .= '           REPOSITORY=$(git rev-parse --show-toplevel)' . "\n";
+        $sExpected .= 'Params     Parameters depending on hook type.' . "\n";
+        $sExpected .= 'Hook       client.pre-commit, server.update' . "\n";
+        $sExpected .= "\n";
+        $sExpected .= 'Examples: ';
+        $sExpected .= '/path/to/hookframework/Hook $REPOSITORY HEAD client.HOOK-IDENTIFIER' . "\n";
+        $sExpected .= '/path/to/hookframework/Hook $REPOSITORY $SHA1 server.HOOK-IDENTIFIER' . "\n";
+
+        $this->assertEquals($sExpected, $oUsage->getUsage());
+    }
+}
