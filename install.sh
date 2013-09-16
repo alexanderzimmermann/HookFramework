d1df7791e011436297f65bc96a7d1c8f153a9756
alexanderzimmermann <alex@azimmermann.com>
2013-09-16 19:56:43 +0200

    Improvement in console output and main routine variable handling. Adding / to the repository directory so that the sed command works.

--- install.sh
+++ install.sh
@@ -15,7 +15,8 @@
 # param $2 string $HOOK_DIR
 # param $3 string $REPOSITORY
 # param $4 string $REPOSITORY_DIR
-# param $5 string $TMP_INSTALL_DIR
+# paara $5 string $VCS
+# param $6 string $EXAMPLE
 #
 
 InstallMain() {
@@ -26,9 +27,11 @@ InstallMain() {
     HOOK_DIR=$2
     REPOSITORY=$3
     REPOSITORY_DIR=$4
-    TMP_INSTALL_DIR=$5
-    VCS=$6
-    EXAMPLE=$7
+    VCS=$5
+    EXAMPLE=$6
+
+    # Escape path for sed using bash find and replace.
+    TMP_INSTALL_DIR="${INSTALL_DIR//\//\\/}"
 
 	# Just copy all templates in the given subversion repository.
 	echo "* Copy hook files to target $HOOK_DIR"
@@ -36,15 +39,15 @@ InstallMain() {
 	cp -v $INSTALL_DIR/Docs/templates/$VCS/* $HOOK_DIR
 
 	# Now replace the "/path/to/hookframework/" path with the actual path of the hook framework.
-	echo "* Replace default path /path/to/hookframework/ with $INSTALL_DIR"
+	echo "* Replace default path /path/to/hookframework/ with $INSTALL_DIR in $HOOK_DIR"
 	echo ""
 	find $HOOK_DIR -type f -exec sed -i 's/\/path\/to\/hookframework\//'"$TMP_INSTALL_DIR"'/g' {} \;
 
 	# Copy the example listener.
-	echo "* Copy example listener $INSTALL_DIR to Repositories/$REPOSITORY"
+	echo "* Copy example listener $INSTALL_DIR"Repositories/"$EXAMPLE/* to Repositories/$REPOSITORY"
 	echo ""
 	mkdir -vp $REPOSITORY_DIR
-	cp -vR "$INSTALL_DIR"Repositories/$EXAMPLE/* $REPOSITORY_DIR
+	cp -vR "$INSTALL_DIR"Repositories/"$EXAMPLE"/* $REPOSITORY_DIR
 	echo ""
 
 	# Parse the listener php files and adjust the namespace.
@@ -66,7 +69,7 @@ InstallSvn() {
 	INSTALL_DIR=`pwd`/
 
 	# Define the hook dir.
-	HOOK_DIR=$1/hooks
+	HOOK_DIR=$1/hooks/
 
 	# Get the new repository name.
 	REPOSITORY=$(basename $1)
@@ -74,15 +77,12 @@ InstallSvn() {
 	# Target repository directory
 	REPOSITORY_DIR="$INSTALL_DIR"Repositories/$REPOSITORY
 
-	## Escape path for sed using bash find and replace
-	TMP_INSTALL_DIR="${INSTALL_DIR//\//\\/}"
-
 
 	# Check that the repository exists.
 	if [ -f $1/format ]
 	then
 		# Copy and setup all stuff.
-		InstallMain $INSTALL_DIR $HOOK_DIR $REPOSITORY $REPOSITORY_DIR $TMP_INSTALL_DIR svn ExampleSvn
+		InstallMain $INSTALL_DIR $HOOK_DIR $REPOSITORY $REPOSITORY_DIR svn ExampleSvn
 	else
 		return 1;
 
@@ -103,7 +103,7 @@ InstallGit() {
 	INSTALL_DIR=`pwd`/
 
 	# Define the hook dir.
-	HOOK_DIR=$1/.git/hooks
+	HOOK_DIR=$1/.git/hooks/
 
 	# Get the new repository name.
 	REPOSITORY=$(basename $1)
@@ -111,15 +111,12 @@ InstallGit() {
 	# Target repository directory
 	REPOSITORY_DIR="$INSTALL_DIR"Repositories/$REPOSITORY
 
-	## Escape path for sed using bash find and replace
-	TMP_INSTALL_DIR="${INSTALL_DIR//\//\\/}"
-
 
 	# Check that the repository exists.
 	if [ -d $HOOK_DIR ]
 	then
 		# Copy and setup all stuff.
-		InstallMain $INSTALL_DIR $HOOK_DIR $REPOSITORY $REPOSITORY_DIR $TMP_INSTALL_DIR git ExampleGit
+		InstallMain $INSTALL_DIR $HOOK_DIR $REPOSITORY $REPOSITORY_DIR git ExampleGit
 	else
 		return 1;
 	fi
@@ -175,3 +172,8 @@ if [ "$return_val" -eq 1 ]
 then
 	ShowUsage $1
 fi
+
+echo ""
+echo "Install completed"
+echo ""
+echo ""
