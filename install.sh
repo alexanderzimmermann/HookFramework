bd458ea43a2db72a272f296b16d8b0133ad5fae5
alexanderzimmermann <alex@azimmermann.com>
2013-09-17 10:10:50 +0200

    Code formatting. Tabs to space.

--- install.sh
+++ install.sh
@@ -21,9 +21,9 @@
 
 InstallMain() {
 
-	clear
+    clear
 
-	INSTALL_DIR=$1
+    INSTALL_DIR=$1
     HOOK_DIR=$2
     REPOSITORY=$3
     REPOSITORY_DIR=$4
@@ -33,28 +33,28 @@ InstallMain() {
     # Escape path for sed using bash find and replace.
     TMP_INSTALL_DIR="${INSTALL_DIR//\//\\/}"
 
-	# Just copy all templates in the given subversion repository.
-	echo "* Copy hook files to target $HOOK_DIR"
-	echo ""
-	cp -v $INSTALL_DIR/Docs/templates/$VCS/* $HOOK_DIR
+    # Just copy all templates in the given subversion repository.
+    echo "* Copy hook files to target $HOOK_DIR"
+    echo ""
+    cp -v $INSTALL_DIR/Docs/templates/$VCS/* $HOOK_DIR
 
-	# Now replace the "/path/to/hookframework/" path with the actual path of the hook framework.
-	echo "* Replace default path /path/to/hookframework/ with $INSTALL_DIR in $HOOK_DIR"
-	echo ""
-	find $HOOK_DIR -type f -exec sed -i 's/\/path\/to\/hookframework\//'"$TMP_INSTALL_DIR"'/g' {} \;
+    # Now replace the "/path/to/hookframework/" path with the actual path of the hook framework.
+    echo "* Replace default path /path/to/hookframework/ with $INSTALL_DIR in $HOOK_DIR"
+    echo ""
+    find $HOOK_DIR -type f -exec sed -i 's/\/path\/to\/hookframework\//'"$TMP_INSTALL_DIR"'/g' {} \;
 
-	# Copy the example listener.
-	echo "* Copy example listener $INSTALL_DIR"Repositories/"$EXAMPLE/* to Repositories/$REPOSITORY"
-	echo ""
-	mkdir -vp $REPOSITORY_DIR
-	cp -vR "$INSTALL_DIR"Repositories/"$EXAMPLE"/* $REPOSITORY_DIR
-	echo ""
+    # Copy the example listener.
+    echo "* Copy example listener $INSTALL_DIR"Repositories/"$EXAMPLE/* to Repositories/$REPOSITORY"
+    echo ""
+    mkdir -vp $REPOSITORY_DIR
+    cp -vR "$INSTALL_DIR"Repositories/"$EXAMPLE"/* $REPOSITORY_DIR
+    echo ""
 
-	# Parse the listener php files and adjust the namespace.
-	echo "* Change namespace in $REPOSITORY_DIR/ from namespace $EXAMPLE to namespace $REPOSITORY"
-	find $REPOSITORY_DIR -type f -exec sed -i 's/'"namespace $EXAMPLE"'/'"namespace $REPOSITORY"'/g' {} \;
+    # Parse the listener php files and adjust the namespace.
+    echo "* Change namespace in $REPOSITORY_DIR/ from namespace $EXAMPLE to namespace $REPOSITORY"
+    find $REPOSITORY_DIR -type f -exec sed -i 's/'"namespace $EXAMPLE"'/'"namespace $REPOSITORY"'/g' {} \;
 
-	return 0;
+    return 0;
 }
 
 
@@ -65,30 +65,30 @@ InstallMain() {
 
 InstallSvn() {
 
-	# Get the install directory.
-	INSTALL_DIR=`pwd`/
+    # Get the install directory.
+    INSTALL_DIR=`pwd`/
 
-	# Define the hook dir.
-	HOOK_DIR=$1/hooks/
+    # Define the hook dir.
+    HOOK_DIR=$1/hooks/
 
-	# Get the new repository name.
-	REPOSITORY=$(basename $1)
+    # Get the new repository name.
+    REPOSITORY=$(basename $1)
 
-	# Target repository directory
-	REPOSITORY_DIR="$INSTALL_DIR"Repositories/$REPOSITORY
+    # Target repository directory
+    REPOSITORY_DIR="$INSTALL_DIR"Repositories/$REPOSITORY
 
 
-	# Check that the repository exists.
-	if [ -f $1/format ]
-	then
-		# Copy and setup all stuff.
-		InstallMain $INSTALL_DIR $HOOK_DIR $REPOSITORY $REPOSITORY_DIR svn ExampleSvn
-	else
-		return 1;
+    # Check that the repository exists.
+    if [ -f $1/format ]
+    then
+        # Copy and setup all stuff.
+        InstallMain $INSTALL_DIR $HOOK_DIR $REPOSITORY $REPOSITORY_DIR svn ExampleSvn
+    else
+        return 1;
 
-	fi
+    fi
 
-	return 0;
+    return 0;
 }
 
 
@@ -99,29 +99,29 @@ InstallSvn() {
 
 InstallGit() {
 
-	# Get the install directory.
-	INSTALL_DIR=`pwd`/
+    # Get the install directory.
+    INSTALL_DIR=`pwd`/
 
-	# Define the hook dir.
-	HOOK_DIR=$1/.git/hooks/
+    # Define the hook dir.
+    HOOK_DIR=$1/.git/hooks/
 
-	# Get the new repository name.
-	REPOSITORY=$(basename $1)
+    # Get the new repository name.
+    REPOSITORY=$(basename $1)
 
-	# Target repository directory
-	REPOSITORY_DIR="$INSTALL_DIR"Repositories/$REPOSITORY
+    # Target repository directory
+    REPOSITORY_DIR="$INSTALL_DIR"Repositories/$REPOSITORY
 
 
-	# Check that the repository exists.
-	if [ -d $HOOK_DIR ]
-	then
-		# Copy and setup all stuff.
-		InstallMain $INSTALL_DIR $HOOK_DIR $REPOSITORY $REPOSITORY_DIR git ExampleGit
-	else
-		return 1;
-	fi
+    # Check that the repository exists.
+    if [ -d $HOOK_DIR ]
+    then
+        # Copy and setup all stuff.
+        InstallMain $INSTALL_DIR $HOOK_DIR $REPOSITORY $REPOSITORY_DIR git ExampleGit
+    else
+        return 1;
+    fi
 
-	return 0;
+    return 0;
 }
 
 
@@ -133,18 +133,18 @@ InstallGit() {
 
 function ShowUsage() {
 
-	vcs=`echo $1 | tr '[:upper:]' '[:lower:]'`
+    vcs=`echo $1 | tr '[:upper:]' '[:lower:]'`
 
-	if [ "$1" = "svn" ]
-	then
-		echo "Usage SVN: [sudo] ./install.sh svn /var/svn/REPOSITORY-NAME"
-	elif [ "$1" = "git" ]
-	then
-		echo "Usage GIT: [sudo] ./install.sh git /path/to/git/directory"
-	else
-		echo "Usage SVN: [sudo] ./install.sh svn /var/svn/REPOSITORY-NAME"
-		echo "Usage GIT: [sudo] ./install.sh git /path/to/git/directory"
-	fi
+    if [ "$1" = "svn" ]
+    then
+        echo "Usage SVN: [sudo] ./install.sh svn /var/svn/REPOSITORY-NAME"
+    elif [ "$1" = "git" ]
+    then
+        echo "Usage GIT: [sudo] ./install.sh git /path/to/git/directory"
+    else
+        echo "Usage SVN: [sudo] ./install.sh svn /var/svn/REPOSITORY-NAME"
+        echo "Usage GIT: [sudo] ./install.sh git /path/to/git/directory"
+    fi
 }
 
 
@@ -170,7 +170,7 @@ esac
 # If an error occurs, show the usage hints.
 if [ "$return_val" -eq 1 ]
 then
-	ShowUsage $1
+    ShowUsage $1
 fi
 
 echo ""
